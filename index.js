de9928827662d8f45325b5964d7c965dc685158c
kathy789 <ji79@purdue.edu>
2017-01-01 23:38:06 -0500

    add username

--- index.js
+++ index.js
@@ -1,22 +1,61 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var nickNames=[];
 
 app.get('/', function(req, res){
 	res.sendfile('index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+
+  /*
+  socket.on('join', function(name){
+  	people[socket.id] = name;
+  	socket.emit("update", "You have connected to the server.");
+  	io.emit("update", name + " has joined the server.")
+    io.emit("update-people", people);
+    
+  });
+  */
   socket.on('chat message', function(msg){
-  	io.emit('chat message', msg);
+   
+  	io.emit('chat message', {nickName: socket.nickName, message: msg });
   });
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  // handle event of new user
+  socket.on('new user', function(nickName, callback){
+
+    if (nickNames.indexOf(nickName) != -1) {
+        console.log("-1 name:" + nickName);
+        callback(false);
+    }
+    else {
+        
+        console.log(" name:" + nickName);       
+        callback(true);
+        socket.nickName = nickName;
+        nickNames.push(nickName);
+        console.log("nickNames" + nickNames.toString());
+        io.emit('usernames', nickNames);
+       
+    }
   });
+  
+  socket.on('disconnect', function(data){
+     console.log('user disconnected');
+     if (!socket.nickName) return;
+     nickNames.splice(nickNames.indexOf(socket.nickName), 1);
+     io.emit('usernames', nickNames);
+       
+  });
+   
+  
+
 });
 
 http.listen(3000, function() {
 	console.log('listening on *:3000');
 });
+
+
